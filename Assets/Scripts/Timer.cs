9c7e1e5624d578db5e0b4a2108fbb76ba560503d
Khairil Azmyr <azmyr36@gmail.com>
2024-08-03 04:02:40 +0800

    Timer and own test scene

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float timeRemaining = 30f;
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private GameObject gameOverCanvas;
+
+    private void Start()
     {
-        
+        gameOverCanvas.SetActive(false);
+        UpdateTimeText();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining < 0) timeRemaining = 0; 
+            UpdateTimeText();
+        }
+        else
+        {
+            gameOverCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    private void UpdateTimeText()
+    {
+        timeText.text = Mathf.CeilToInt(timeRemaining).ToString();
     }
 }
