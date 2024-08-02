630ca37db68a5206b4313e731743db78d4f07165
Khairil Azmyr <azmyr36@gmail.com>
2024-08-02 22:49:11 +0800

    Update again

--- Assets/Scripts/ObjectTapped.cs
+++ Assets/Scripts/ObjectTapped.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class ObjectTapped : MonoBehaviour
 {
+    private Spawner spawner;
+    private Transform spawnPoint;
+
+    public void Initialize(Spawner spawner, Transform spawnPoint)
+    {
+        this.spawner = spawner;
+        this.spawnPoint = spawnPoint;
+    }
+
     void OnMouseDown()
     {
-        // Destroy the game object this script is attached to
-        Destroy(gameObject);
+        spawner.ObjectDestroyed(spawnPoint);
+        gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,12 +6,35 @@ public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject objectToSpawn;
+    public float respawnDelay = 2f;
+
 
     void Start()
     {
         foreach (Transform spawnPoint in spawnPoints)
         {
-            Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
+            SpawnObjectAt(spawnPoint);
+        }
+    }
+
+    public void ObjectDestroyed(Transform spawnPoint)
+    {
+        StartCoroutine(RespawnAfterDelay(spawnPoint));
+    }
+
+    private IEnumerator RespawnAfterDelay(Transform spawnPoint)
+    {
+        yield return new WaitForSeconds(respawnDelay);
+        SpawnObjectAt(spawnPoint);
+    }
+
+    private void SpawnObjectAt(Transform spawnPoint)
+    {
+        GameObject obj = Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
+        ObjectTapped tapToDisappear = obj.GetComponent<ObjectTapped>();
+        if (tapToDisappear != null)
+        {
+            tapToDisappear.Initialize(this, spawnPoint);
         }
     }
 }
